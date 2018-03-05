94facf2c44b066bee974213f7dab406a6e2b4496
https://github.com/samantha-yee/bcitease.git <a2944333850@gmail.com>
2018-03-04 23:27:22 -0800

    add

--- beautytwin/src/index.js
+++ beautytwin/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import Login from './comp/Login';
 import registerServiceWorker from './registerServiceWorker';
 
